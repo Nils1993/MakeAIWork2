496ce2dd33410899cbd3b3c90ec7c430784933a5
Nils1993 <1069690@hr.nl>
2023-04-25 10:00:14 +0200

    tweaks

--- project/simpylc/start_nn.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-export PYTHONPATH=simulations/car
-export LIBGL_ALLOW_SOFTWARE=1
-
-python simulations/car/control_client/neural_network_client.py
--- project/simpylc/test_nn.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-export PYTHONPATH=simulations/car
-export LIBGL_ALLOW_SOFTWARE=1
-
-(sleep 3; ./start_nn.sh)&
-./start_world.sh
